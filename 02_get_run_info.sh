34dc3f7610ac86b13c178ea5111d2ea0e01b9a26
Gennaro Calendo <gcalendo@coriell.org>
2021-02-22 14:02:18 -0500

    added loop for getting additional sample metadata

--- 02_get_run_info.sh
+++ 02_get_run_info.sh
@@ -20,3 +20,24 @@ while read -r BIOPROJECT N_SAMPLES TITLE; do
         esearch -db sra -query \"$BIOPROJECT\" < /dev/null | efetch -format runinfo > $OUT_DIR/$BIOPROJECT/run-info.csv
     fi
 done < $SEARCH_RESULTS
+
+# Get additional metadata not present in run_info.csv
+while read -r BIOPROJECT N_SAMPLES TITLE; do
+    if [ $N_SAMPLES -gt $MIN_SAMPLES -a $N_SAMPLES -lt $MAX_SAMPLES ] 
+    then
+        sleep 1
+        esearch -db sra -query \"$BIOPROJECT\" </dev/null | \
+            efetch -format native | \
+            xtract -pattern EXPERIMENT_PACKAGE \
+                -group RUN_SET \
+                    -block RUN \
+                        -block IDENTIFIERS \
+                            -element PRIMARY_ID \
+                -group SAMPLE \
+                    -block SAMPLE_ATTRIBUTES \
+                        -block SAMPLE_ATTRIBUTE \
+                            -element VALUE \
+                -group EXPERIMENT \
+                    -element TITLE > $OUT_DIR/$BIOPROJECT/metadata.txt
+    fi
+done < $SEARCH_RESULTS
